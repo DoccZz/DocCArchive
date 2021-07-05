d32d3acd345700d8622c43e651084b4b836cac39
Helge Heß <devteam@zeezide.de>
2021-07-05 17:10:54 +0200

    Add more `description`s
    Eventually we'll have all of them.

--- Sources/DocCArchive/Schema_0_1/Chapter.swift
+++ Sources/DocCArchive/Schema_0_1/Chapter.swift
@@ -11,11 +11,21 @@ extension DocCArchive.DocCSchema_0_1 {
   /**
    * Chapter in a `volume` ``Section``.
    */
-  public struct Chapter: Equatable, Codable {
+  public struct Chapter: Equatable, CustomStringConvertible, Codable {
     
-    public var name     : String
-    public var image    : String?
-    public var content  : [ Content ]
-    public var tutorials : [ Identifier ] // TBD
+    public var name      : String
+    public var image     : String?
+    public var content   : [ Content    ]
+    public var tutorials : [ Identifier ]
+
+    public var description: String {
+      var ms = "<Chapter:"
+      if !name.isEmpty      { ms += " “\(name)”"    }
+      if let s = image      { ms += " image=\(s)"   }
+      if !content .isEmpty  { ms += " \(content)"   }
+      if !tutorials.isEmpty { ms += " \(tutorials)" }
+      ms += ">"
+      return ms
+    }
   }
 }
--- Sources/DocCArchive/Schema_0_1/Sections/VolumeSection.swift
+++ Sources/DocCArchive/Schema_0_1/Sections/VolumeSection.swift
@@ -8,11 +8,21 @@
 
 extension DocCArchive.DocCSchema_0_1.Section {
 
-  public struct Volume: Equatable, Codable {
+  public struct Volume: Equatable, CustomStringConvertible, Codable {
     
     public var name     : String?
     public var image    : String?
     public var content  : [ DocCArchive.DocCSchema_0_1.Content ]
     public var chapters : [ DocCArchive.DocCSchema_0_1.Chapter ]
+    
+    public var description: String {
+      var ms = "<Volume:"
+      if let s = name      { ms += " “\(s)”"      }
+      if let s = image     { ms += " image=\(s)"  }
+      if !content .isEmpty { ms += " \(content)"  }
+      if !chapters.isEmpty { ms += " \(chapters)" }
+      ms += ">"
+      return ms
+    }
   }
 }
